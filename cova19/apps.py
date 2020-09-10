3bc566d1bd781946cee7762fe0559d00478659d7
cadmiel <cadimiel.donato@sistemafiep.org.br>
2020-09-10 19:42:22 -0300

    commit

---
+++ cova19/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Cova19Config(AppConfig):
+    name = 'cova19'
--- projectCovid/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('', views.index, name='index'),
-]
